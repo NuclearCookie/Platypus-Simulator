03d50b92b4c15dde49f41e33bf149a62900bd9b0
NuclearCookie <pietervantorre@gmail.com>
2014-10-23 22:30:46 +0200

    The platypus can now be controlled using the phone's gyroscope

--- Assets/_Scripts/StartGame.cs
+++ Assets/_Scripts/StartGame.cs
@@ -3,21 +3,26 @@ using System.Collections;
 
 public class StartGame : MonoBehaviour {
 
-	int inputs = 0;
+    int inputs = 0;
 
-	// Update is called once per frame
-	void Update ()
-	{
-		if(Input.anyKey
-			&& !Input.GetButton("Oculus"))
-		{
-			inputs++;
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
 
-			if(inputs >= 2)
-			{
-				int nextLevel = Application.loadedLevel + 1;
-				Application.LoadLevel(nextLevel);
-			}
-		}
-	}
+    // Update is called once per frame
+    void Update ()
+    {
+        if(Input.anyKey
+            && !Input.GetButton("Oculus"))
+        {
+            inputs++;
+
+            if(inputs >= 2)
+            {
+                int nextLevel = Application.loadedLevel + 1;
+                Application.LoadLevel(nextLevel);
+            }
+        }
+    }
 }
--- Assets/scripts/FirstPersonCamera.cs
+++ Assets/scripts/FirstPersonCamera.cs
@@ -2,39 +2,45 @@
 using System.Collections;
 
 public class FirstPersonCamera : MonoBehaviour {
-	
-	public float sensitivityX = 4.0f;
-	public float sensitivityY = 4.0f;
-	public float minimumY = -60f;
-	public float maximumY = 60f;
-	public bool flipY = false;
-	public bool flipX = false;
-	
-	float rotationY = 0f;
-	// Use this for initialization
-	void Start () {
-		Screen.showCursor = false;
-	}
-	
-	
-	/// Update is called once per frame
-	void Update ()
-	{		
-		float rotationX = 0;
-		if(!flipX)
-			rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-		else
-			rotationX = transform.localEulerAngles.y - Input.GetAxis("Mouse X") * sensitivityX;
-		
-		if(!flipY)
-			rotationY  += Input.GetAxis("Mouse Y") * sensitivityY;
-		else
-			rotationY  -= Input.GetAxis("Mouse Y") * sensitivityY;
-		
-		rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
-		
-		transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-	}
-	
-	void OnApplicationFocus(bool focus){ Screen.showCursor = false; }
+    
+    public float sensitivityX = 4.0f;
+    public float sensitivityY = 4.0f;
+    public float minimumY = -60f;
+    public float maximumY = 60f;
+    public bool flipY = false;
+    public bool flipX = false;
+    private float rotationY = 0.0f;
+    
+    // Use this for initialization
+    void Start () {
+        Input.gyro.enabled = true;
+        Screen.showCursor = false;
+    }
+
+    /// Update is called once per frame
+    void Update()
+    {
+        float rotationX = 0;
+        
+        if (!flipX)
+            rotationX = transform.localEulerAngles.y + Input.gyro.rotationRateUnbiased.x * sensitivityX;
+        else
+            rotationX = transform.localEulerAngles.y - Input.gyro.rotationRateUnbiased.x * sensitivityX;
+
+        if (!flipY)
+            rotationY += Input.gyro.rotationRateUnbiased.y * sensitivityY;
+        else
+            rotationY -= Input.gyro.rotationRateUnbiased.y * sensitivityY;
+
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            Debug.Log(Input.gyro.rotationRateUnbiased);
+        }
+    }
+    
+    void OnApplicationFocus(bool focus){ Screen.showCursor = false; }
 }
