b152c0ba7f62c564649fcdeed9568cc43139cc21
NuclearCookie <pietervantorre@gmail.com>
2014-10-28 18:26:08 +0100

    Added Layer specific range culling. Changed all prefab layers so that they are being culled according to their importance and in game size.

---
+++ Assets/scripts/CameraCulling.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCulling : MonoBehaviour {
+
+    public float[] DistancesPerLayer = new float[32];
+    // Use this for initialization
+    void Start () {
+        gameObject.GetComponent<Camera>().layerCullDistances = DistancesPerLayer;
+    }
+}
--- Assets/scripts/GlobalSettings.cs
+++ Assets/scripts/GlobalSettings.cs
@@ -4,42 +4,42 @@ using System.Collections;
 public class GlobalSettings : MonoBehaviour
 {
     
-	private static GlobalSettings mInstance = null;
-	
-	private static GlobalSettings instance
-	{
-		get
-		{
-			if( mInstance == null )
-			{
-				mInstance = GameObject.FindObjectOfType(typeof(GlobalSettings)) as GlobalSettings;
+    private static GlobalSettings mInstance = null;
+    
+    private static GlobalSettings instance
+    {
+        get
+        {
+            if( mInstance == null )
+            {
+                mInstance = GameObject.FindObjectOfType(typeof(GlobalSettings)) as GlobalSettings;
  
-				if( mInstance == null )
-				{
-					mInstance = new GameObject("GlobalSettings").AddComponent<GlobalSettings>();
-				}
-			}
+                if( mInstance == null )
+                {
+                    mInstance = new GameObject("GlobalSettings").AddComponent<GlobalSettings>();
+                }
+            }
  
-			return mInstance;
-		}
-	}
-	
-	
-	void Awake()
-	{
-		if( mInstance == null )
-		{
-			mInstance = this as GlobalSettings;
-			//instance.init();
-		}
-	}
-	
-	public bool UseOcculusRift = true;
-	public bool UseMicrophone = true;
-	
-	public static bool GetUsingOcculusRift() { return instance.UseOcculusRift; }
-	public static bool GetUsingMicrophone() { return instance.UseMicrophone; }
-	public static void ToggleUsingOcculusRift() { instance.UseOcculusRift = !instance.UseOcculusRift; }
-	public static void DisableUsingOcculusRift() { instance.UseOcculusRift = false; }
-	public static void EnableUsingOcculusRift() { instance.UseOcculusRift = true; }
+            return mInstance;
+        }
+    }
+    
+    
+    void Awake()
+    {
+        if( mInstance == null )
+        {
+            mInstance = this as GlobalSettings;
+            //instance.init();
+        }
+    }
+    
+    public bool UseOcculusRift = true;
+    public bool UseMicrophone = true;
+    
+    public static bool GetUsingOcculusRift() { return instance.UseOcculusRift; }
+    public static bool GetUsingMicrophone() { return instance.UseMicrophone; }
+    public static void ToggleUsingOcculusRift() { instance.UseOcculusRift = !instance.UseOcculusRift; }
+    public static void DisableUsingOcculusRift() { instance.UseOcculusRift = false; }
+    public static void EnableUsingOcculusRift() { instance.UseOcculusRift = true; }
 }
